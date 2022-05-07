951c5863a24df3a52fa8deaa6329c027afa1a628
Jacob Doren <jakedoren@gmail.com>
2022-05-06 22:13:27 -0400

    inital commit, vue js intro practice

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+  const res = ref(['Betty', 'Jake', 'Meagan', "Bella"])
+  console.log(res)
+</script>
+
+<template>
+  <input type="text" :value="res[2]">
+</template>
+
+<style>
+  .red {
+    color: purple;
+  }
+</style>
